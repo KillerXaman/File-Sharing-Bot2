a817ab259ab4789432e09c0b0d6a0f01bcfb0a8e
KillerXaman <102473258+KillerXaman@users.noreply.github.com>
2025-08-02 23:09:04 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -63,6 +63,26 @@ async def link_generator(client: Client, message: Message):
 
 
 
+@Bot.on_message(filters.private & filters.command("clink"))
+async def custom_link_command(client, message):
+    try:
+        # Command ke baad ka data lo
+        data = message.text.split(" ", 1)[1]
+        thumb_url, caption, btn_text, btn_url = [i.strip() for i in data.split("|")]
+
+        await message.reply_photo(
+            photo=thumb_url,
+            caption=f"<b>{caption}</b>",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton(btn_text, url=btn_url)]]
+            )
+        )
+
+    except Exception as e:
+        await message.reply_text(
+            "❌ Format error!\n\nUse like:\n<code>/clink thumbnail_url | Caption | Button Text | Button Link</code>"
+        )
+        
 
 
 
